0de45c9580e7eca5da84a0ab85fa6692cdedc327
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-10-30 11:37:54 +0000

    Removed legacy context_processor

--- _context_processors/dashboard.py
+++
@@ -1,7 +0,0 @@
-def user_details(request):
-    if request.user.is_authenticated():
-        return {
-            'user_full_name': request.user.get_full_name,
-        }
-    else:
-        return {}
