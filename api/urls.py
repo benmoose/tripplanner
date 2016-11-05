b2f2de0e380861357080ba973099bc642a07cf6a
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-11-05 11:42:04 +0000

    Cleanup

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.authtoken import views as authoken_views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from trip import views as trip_views
-from user import views as user_views
 from user_jwt import views as user_jwt_views
 
 from user_jwt.utility.jwt_authentication import requires_login
@@ -14,11 +13,9 @@ app_name = 'api'
 urlpatterns = [
     url(r'^api-token-auth/?$', authoken_views.obtain_auth_token),
 
-    url(r'^user/?$', requires_login(user_views.UserDetail.as_view())),
-
     url(r'^user/jwt/get-or-create/?$', requires_login(user_jwt_views.GetOrCreateUserJWT.as_view())),
 
-    url(r'^trips/?$', trip_views.TripList.as_view()),
+    url(r'^trips/?$', requires_login(trip_views.TripList.as_view())),
     url(r'^trips/(?P<uuid>[\w-]+)/?$', requires_login(trip_views.TripDetail.as_view())),
 ]
 
--- trip/views.py
+++ trip/views.py
@@ -27,11 +27,14 @@ class TripList(generics.ListAPIView):
         This ensures that user's can only see trips they are involved with.
         """
         return queryset.filter(
-            users__username__contains=self.request.user.username)
+            users__sub=header_to_sub(
+                self.request.META.get('HTTP_AUTHORIZATION')))
 
 
 class TripDetail(generics.RetrieveAPIView):
-    """Returns details for a single trip."""
+    """
+    Returns details for a single trip.
+    """
     queryset = Trip.objects.filter(active=True)
     serializer_class = TripSerializer
     lookup_field = 'uuid'
