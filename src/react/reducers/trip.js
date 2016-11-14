67a9926ff5b4f5d85ff27860e526d1b2f9a31048
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2016-11-14 10:12:30 +0000

    Added flow to trip reducer

--- src/react/reducers/trip.js
+++ src/react/reducers/trip.js
@@ -1,19 +1,36 @@
 /*
+ * @flow
+ *
  * Redux Trip Reducer
  * */
 
 import * as actionType from '../actions/trip';
+import * as T from '../types/';
 
 
-const defaultState = {
-    loading: false,
-    error: undefined,
+type State = {
+    uuid: ?string,
+    title: ?string,
+    get_absolute_url: ?string,
+    locations: Array<T.Location>,
+    created: ?string,
+    modified: ?string,
+    loading: boolean,
+    error: ?string,
+}
+
+const defaultState: State = {
     uuid: undefined,
     title: undefined,
+    get_absolute_url: null,
     locations: [],
+    created: null,
+    modified: null,
+    loading: false,
+    error: null,
 };
 
-export const tripReducer = (state = defaultState, action) => {
+export const tripReducer = (state: State = defaultState, action: Object): State => {
     switch (action.type) {
         case actionType.TRIP_REQUEST:
             return {
--- src/react/types/trip.js
+++ src/react/types/trip.js
@@ -1,19 +1,30 @@
 /*
-* @flow
-*
-* Trip Types
-* */
+ * @flow
+ *
+ * Trip Types
+ */
 
-
-export type Trip = {
-    title: string,
-    get_absolute_url: string,
-    locations: Array<{}>,
-    created: string,
-    modified: string,
-}
 
 export type TripSimple = {
     title: string,
     uuid: string,
+}
+
+export type Trip = {
+    uuid: string,
+    title: string,
+    get_absolute_url: string,
+    locations: Array<Location>,
+    created: string,
+    modified: string,
+}
+
+export type Location = {
+    arrive: ?string,
+    depart: ?string,
+    latitude: string,
+    longitude: string,
+    title: string,
+    travel_icon: string,
+    travel_type: string,
 }
